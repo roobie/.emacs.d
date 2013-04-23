57527b9518901939633bf3959658802ebf1a4ae1
roobie <bjorn.roberg@gmail.com>
2013-04-23 19:22:58 +0200

    fix

--- key-bindings.el
+++ key-bindings.el
@@ -1,18 +1,6 @@
-;;; These alters the default functionality of the cut and copy commands in Emacs
-;;; so that it acts on the current line if no region is visually selected.
-(put 'kill-ring-save 'interactive-form
-     '(interactive
-       (if (use-region-p)
-           (list (region-beginning) (region-end))
-         (list (line-beginning-position) (line-beginning-position 2)))))
 
-(put 'kill-region 'interactive-form
-     '(interactive
-       (if (use-region-p)
-           (list (region-beginning) (region-end))
-         (list (line-beginning-position) (line-beginning-position 2)))))
-;;; End alter default funtionality.
 
-(global-set-key (kbd "C-w") 'kill-whole-line)
+
+;(global-set-key (kbd "C-w") 'kill-whole-line-or-region)
 
 (provide 'key-bindings)
