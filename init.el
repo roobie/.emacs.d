bdd278851deb398a7dc3f97acec6ea0f2c3ef654
Björn Roberg <bjorn.roberg@gmail.com>
2013-12-27 13:43:17 +0100

    streamlined init.el a bit

--- init.el
+++ init.el
@@ -1,26 +1,33 @@
 
 ;;; BOOTSTRAP
-;; Load the files in the .emacs.d directory
-(add-to-list 'load-path (expand-file-name "~/.emacs.d"))
 
-;;; require the applicalble configurations
-(require 'gui) ; fix stuff like menu bar and font
-(require 'utils)
-(require 'pkg) ; initialize package handling and install missing default packages
-(require 'misc) ; misc stuff like preferences and the like
-(require 'theming) ; initialize sane themes
-(require 'key-bindings) ; manage key bindings
-(require 'auto-complete)
-(require 'idocfg)
-(require 'lisp)
-(require 'slime)
-(require 'javascript)
-(require 'go)
-(require 'go-autocomplete)
-(require 'auto-complete-config)
+(defvar *load-files* '(gui
+                       utils
+                       pkg
+                       misc
+                       theming
+                       key-bindings
+                       auto-complete
+                       idocfg
+                       lisp
+                       slime
+                       javascript
+                       go
+                       go-autocomplete
+                       ;auto-complete-config
+                       ))
+
+;; Load the files in the .emacs.d directory
+(defvar *base-path* (expand-file-name "~/.emacs.d"))
+(dolist (file *load-files*)
+  (load (format "%s/%s" *base-path* file))
+  (require file))
 
 ;;; Local changes:
 ;; (create a 'local.el' file with custom code for it to be loaded:
 ;; NB: you need to (provide 'local) at the bottom of the file.
 (when (file-exists-p (expand-file-name "~/.emacs.d/local.el"))
-  (require 'local))
+  (progn
+    (load (expand-file-name "~/.emacs.d/local.el"))
+    (require 'local)))
+
